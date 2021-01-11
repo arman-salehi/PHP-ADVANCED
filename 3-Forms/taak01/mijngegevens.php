7fef3cd588f1dd32631f4ddefab0f876dd9db92e
Arman Salehi <arman.salehi@student.rocva.nl>
2021-01-11 11:15:51 +0100

    form1

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Voornaam: " . $_POST["Voornaam"];
+    ?>
+    <br>
+    <?php
+        echo "Achternaam: " . $_POST["Achternaam"];
+    ?>
+    <br>
+    <?php
+        echo "Leeftijd: " . $_POST["Leeftijd"];
+    ?>
+    <br>
+    <?php
+        echo "Klas: " . $_POST["Klas"];
+    ?>
+    <br>
+    <?php
+        echo "Adres: " . $_POST["Adres"];
+    ?>
+    <br>
+    <?php
+        echo "Plaatsnaam: " . $_POST["Plaatsnaam"];
+    ?>
+    <br>
+    <?php
+        echo "Favoriete muziekband: " . $_POST["Favmusic"];
+    ?>
+</body>
+</html>
